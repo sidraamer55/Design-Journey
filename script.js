65db6456f60574ea89e30f5884adcd5e80c06fe3
sidra <sidraamer125@gmail.com>
2024-12-31 22:05:44 +0200

    edit files

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ window.addEventListener('load', function() {
   
   
   
-  
-  
-  
   // Handle mouse movement to create a fog effect
   document.addEventListener('mousemove', function (s) {
     const fogOverlay = document.querySelector('.fog-overlay');
@@ -17,4 +14,8 @@ window.addEventListener('load', function() {
     // Update the background of the fog layer with a radial gradient
     fogOverlay.style.background = `radial-gradient(circle at ${offsetX}px ${offsetY}px, transparent 30%, rgba(0, 0, 0, 0.8) 50%)`;
   });
+
+
+var loader= document.getElementById("preloader");
+window.addEventListener("load", function(){loader.style.display = "none";})
   
