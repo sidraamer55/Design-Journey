6f49f677b5c0edb9299c48a8a70a55b1d82063ab
sidra <sidraamer125@gmail.com>
2024-12-31 21:24:35 +0200

    edit style

--- script.js
+++ script.js
@@ -10,47 +10,11 @@ window.addEventListener('load', function() {
   
   
   // Handle mouse movement to create a fog effect
-  document.addEventListener('mousemove', function (e) {
+  document.addEventListener('mousemove', function (s) {
     const fogOverlay = document.querySelector('.fog-overlay');
-    const { offsetX, offsetY } = e;
+    const { offsetX, offsetY } = s;
   
     // Update the background of the fog layer with a radial gradient
     fogOverlay.style.background = `radial-gradient(circle at ${offsetX}px ${offsetY}px, transparent 30%, rgba(0, 0, 0, 0.8) 50%)`;
   });
-  
-  
-  
-  
-  
-  
-  
-  
-  // JavaScript to toggle play/pause functionality
-  const audioButton = document.getElementById('audioButton');
-  const audioPlayer = document.getElementById('audioPlayer');
-  const playIcon = document.getElementById('playIcon');
-  const pauseIcon = document.getElementById('pauseIcon');
-  
-  audioButton.addEventListener('click', () => {
-    if (audioPlayer.paused) {
-      audioPlayer.play();
-      audioButton.classList.add('playing'); // Add playing state
-      playIcon.style.display = 'none';
-      pauseIcon.style.display = 'block';
-    } else {
-      audioPlayer.pause();
-      audioButton.classList.remove('playing'); // Remove playing state
-      playIcon.style.display = 'block';
-      pauseIcon.style.display = 'none';
-    }
-  });
-  
-  // Optional: Reset button state when audio ends (if not looping)
-  audioPlayer.addEventListener('ended', () => {
-    audioButton.classList.remove('playing');
-    playIcon.style.display = 'block';
-    pauseIcon.style.display = 'none';
-  });
-  
-  
   
